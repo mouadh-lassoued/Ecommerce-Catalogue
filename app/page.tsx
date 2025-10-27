036236c0d2455db934f56e10d3f56aa084f6756d
Mouadh Lassoued <mlassoued14@gmail.com>
2025-10-27 20:46:31 +0100

    beta frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 import ProductsList from "../components/ui/ProductList"
 import TheHeader from "../components/ui/theHeader"
+import { Checkbox } from "@/components/ui/checkbox"
 
-export default function page (){
+type Product ={
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export default async function page (){
+  const products = await fetch('https://fakestoreapi.com/products')    .then(res => res.json())
+  const categories = Array.from(new Set(products.map((p:Product) => p.category))) as string[];
   return (<div className="bg-[#393E46]">
     <TheHeader/>
-    <div className="flex "><div className="w-200 bg-[#F3F2EC] mt-10 m-3  rounded-lg"><div className="text-semibold p-20 pt-5 pb-5 bg-[#1E93AB] rounded-t-lg">Filters:</div></div>
+    <div className="flex "><div className="w-200 bg-[#F3F2EC] mt-10 m-3  rounded-lg">
+      <div className="text-semibold p-20 pt-5 pb-5 bg-[#1E93AB] rounded-t-lg">Filters:</div>
+      <div className="flex items-center gap-3">
+        <ul>
+          {categories.map((category:string)=>(<li key={category} className="p-3"><Checkbox/>{category}</li>))}
+        </ul>
+                </div>
+      </div>
     <div className="m-10"><ProductsList/></div>
     </div>
     </div>
--- components/ui/theHeader.tsx
+++ components/ui/theHeader.tsx
@@ -11,7 +11,8 @@ export default function TheHeader() {
         <input className="w-150 h-10 rounded-l-lg bg-[#DCDCDC] text-1.7xl" type="search" placeholder="   Looking for something ?" />    
         <button className="flex h-10 items-center gap-2 px-4 py-2 bg-[#1E93AB]  rounded-r-lg hover:bg-gray-600 transition-colors duration-200">
             <Search className="w-5 h-5" />
-        </button></div>    
+        </button></div>
+        <div className="text-2xl text-center font-bold  text-[#1E93AB] m-20 h-50 w-50 tracking-wide">call us : +*** ****************</div>    
     </div>
   )
 }
